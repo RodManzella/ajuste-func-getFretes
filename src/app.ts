bc90823ae5efca6687b84e18f9973daa4776e3d8
RodManzella <manzellateixeira@gmail.com>
2025-12-28 19:52:17 -0300

    beginning

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import {errorHandler} from './middlewares/errorHandler';
+
+const app = express();
+app.use(express.json());
+
+
+//Routes
+
+
+
+//EndRoutes---
+
+app.use(errorHandler);
+
+export default app;
